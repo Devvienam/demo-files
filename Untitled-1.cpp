f325c4ecf1fb4b0e398a4e4bdc5df34180b2de27
Devvienam <95991508+Devvienam@users.noreply.github.com>
2023-05-11 17:43:10 +0700

    Update Untitled-1.cpp

--- Untitled-1.cpp
+++ Untitled-1.cpp
@@ -1,9 +1,9 @@
 #include <bits/stdc++.h>
 using namespace std;
-#define ll long long;
-
+#define long long ll;
 int main() {
 	ll n;
 	cin >> n;
-
+	cout << (n ^ 1 && n/2 ^ 1) ? true : false;
+	return 0;
 }
