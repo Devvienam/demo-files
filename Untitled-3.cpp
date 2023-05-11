9ad876f95fe27f84b6f1d0b0f9c917bdff67017a
Hong A <nguyenhonganh2357@example.com>
2023-05-11 17:33:43 +0700

    MOI CAP NHAT

---
+++ Untitled-3.cpp
@@ -0,0 +1,17 @@
+#include <bits/stdc++.h>
+using namespace std;
+string a[1008];
+
+int main() {
+    int n;
+    string s;
+    cin >> n;
+    for (int i = 0; i < n; i++) {
+        cin >> s;
+        if (s.size() > 10) {
+            cout << s[0] << s.size()-2 << s[s.size()-1] << endl;
+            continue;
+        }            
+        cout << s << endl;
+    }
+}
